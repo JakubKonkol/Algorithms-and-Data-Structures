dac2ff4a54ca6ebf852e6139182066b40fb049b8
JakubKonkol <s24406@pjwstk.edu.pl>
2022-11-20 11:20:28 +0100

    done!

--- SortingAlgorithms.py
+++ SortingAlgorithms.py
@@ -1,5 +1,5 @@
 # Jakub Konkol S24406
-# HeapSort & QuickSort & BubbleSort
+# HeapSort & QuickSort & MergeSort
 import math
 import random
 import time
@@ -20,12 +20,12 @@ def giveMeHugeArray():
     array = []
     numberOfElements = random.randint(100000, 1000000)
     for x in range(0, numberOfElements):
-        array.append(random.randint(0, 5000))
+        array.append(random.randint(0, 100000))
     return array
-#generowanie malej tablicy w ramach testu
+#generowanie malej tablicy w ramach testów
 def giveMeSmallArray():
     array = []
-    numberOfElements = random.randint(10, 1000)
+    numberOfElements = random.randint(10, 100000)
     for x in range(0, numberOfElements):
         array.append(random.randint(0, 100))
     return array
@@ -40,8 +40,9 @@ def quickSort(array, left, right):
 
 
 def partition(array, left, right):
-    pivot = array[right]
-    # pivot = (array[right] + array[left]) / 2
+    # pivot = array[right]
+    # pivot = math.floor((array[right] + array[left]) / 2)
+    pivot = array[random.randint(left, right)]
     smaller = left
     for x in range(left, right):
         if array[x] <= pivot:
@@ -53,7 +54,7 @@ def partition(array, left, right):
 ################################################HeapSort################################################################
 def heapSort(array):
     n = len(array)
-    for i in range(n//2 -1, -1, -1):
+    for i in range(n//2 - 1, -1, -1):
         heapify(array, n, i)
     for i in range(n-1, 0, -1):
         array[i], array[0] = array[0], array[i]
@@ -72,64 +73,88 @@ def heapify(array, n, i):
         array[i], array[largest] = array[largest], array[i]
         heapify(array, n, largest)
 
-################################################BubbleSort##############################################################
-def bubbleSort(array):
-    n = len(array)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if array[j] > array[j+1]:
-                array[j], array[j+1] = array[j+1], array[j]
+################################################MergeSort##############################################################
+def mergeSort(array):
+    if len(array) > 1:
+        mid = len(array) // 2
+        left = array[:mid]
+        right = array[mid:]
+        mergeSort(left)
+        mergeSort(right)
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                array[k] = left[i]
+                i += 1
+            else:
+                array[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            array[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            array[k] = right[j]
+            j += 1
+            k += 1
     return array
-########################################################################################################################
-# arrayToSort = giveMeHugeArray()
-arrayToSort = giveMeSmallArray()
+#######################################################################################################################
+arrayToSort = giveMeHugeArray()
+# arrayToSort = giveMeSmallArray()
 arrayLenght = len(arrayToSort)
-
-
+arrayToSortHS = arrayToSort
+arrayToSortBS = arrayToSort
+print("Wielkość tablicy: ", arrayLenght, "\nrozpoczynam sortowanie...")
 # QuickSort
 t = time.process_time()
 sortedArray = quickSort(arrayToSort, 0, arrayLenght-1)
 qs_execution_time = time.process_time() - t
-print(sortedArray)
+print("skonczylem quicksort 1/3")
+# print(sortedArray)
 
 t = time.process_time()
 quickSort(sortedArray, 0, arrayLenght-1)
 qs2_execution_time = time.process_time() - t
-
+print("skonczylem quicksort 2/3")
 sortedArray.reverse()
 t = time.process_time()
 quickSort(sortedArray, 0, arrayLenght-1)
 qs3_execution_time = time.process_time() - t
+print("skonczylem quicksort 3/3")
 
 # HeapSort
 t = time.process_time()
-sortedArray = heapSort(arrayToSort)
+sortedArray = heapSort(arrayToSortHS)
 hs_execution_time = time.process_time() - t
-print(sortedArray)
+print("skonczylem HeapSort 1/3")
 
 t = time.process_time()
 heapSort(sortedArray)
 hs2_execution_time = time.process_time() - t
-
+print("skonczylem HeapSort 2/3")
 sortedArray.reverse()
 t = time.process_time()
 heapSort(sortedArray)
 hs3_execution_time = time.process_time() - t
+print("skonczylem HeapSort 3/3")
 
 #BubbleSort
 t = time.process_time()
-sortedArray = bubbleSort(arrayToSort)
-bs_execution_time = time.process_time() - t
-print(sortedArray)
+sortedArray = mergeSort(arrayToSortBS)
+ms_execution_time = time.process_time() - t
+print("skonczylem MergeSort 1/3")
 
 t = time.process_time()
-bubbleSort(sortedArray)
-bs2_execution_time = time.process_time() - t
+mergeSort(sortedArray)
+ms2_execution_time = time.process_time() - t
+print("skonczylem MergeSort 2/3")
 
 sortedArray.reverse()
 t = time.process_time()
-bubbleSort(sortedArray)
-bs3_execution_time = time.process_time() - t
+mergeSort(sortedArray)
+ms3_execution_time = time.process_time() - t
+print("skonczylem MergeSort 3/3")
 
 msg("QuickSort")
 print("Czas pierwszego sortowania (pierwotna tablica): ", qs_execution_time, "s")
@@ -141,8 +166,8 @@ print("Czas pierwszego sortowania (pierwotna tablica): ", hs_execution_time, "s"
 print("Czas drugiego sortowania (tablica posortowana): ", hs2_execution_time, "s")
 print("Czas trzeciego sortowania (tablica posortowana odwrócona): ", hs3_execution_time, "s")
 print("ilosc elementów w tablicy: ", arrayLenght)
-msg("bubbleSort")
-print("Czas pierwszego sortowania (pierwotna tablica): ", bs_execution_time, "s")
-print("Czas drugiego sortowania (tablica posortowana): ", bs2_execution_time, "s")
-print("Czas trzeciego sortowania (tablica posortowana odwrócona): ", bs3_execution_time, "s")
+msg("MergeSort")
+print("Czas pierwszego sortowania (pierwotna tablica): ", ms_execution_time, "s")
+print("Czas drugiego sortowania (tablica posortowana): ", ms2_execution_time, "s")
+print("Czas trzeciego sortowania (tablica posortowana odwrócona): ", ms3_execution_time, "s")
 print("ilosc elementów w tablicy: ", arrayLenght)
